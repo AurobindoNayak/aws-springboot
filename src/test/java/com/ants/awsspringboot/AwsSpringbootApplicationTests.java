fccd83fa9c43963b5630c69d2ea7603e3f9d831b
aru <aurobindo.nayak795@outlook.com>
2023-04-29 22:41:49 +0530

    adding the sample springboot hello world code to remote repo

---
+++ src/test/java/com/ants/awsspringboot/AwsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ants.awsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
